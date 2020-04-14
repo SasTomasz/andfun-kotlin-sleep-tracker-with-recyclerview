648e74e09c5a04d1dfc4d932b810469e3c84080c
sasTomasz <boss7@op.pl>
2020-04-14 16:49:59 +0200

    resolve all todos

--- app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/BindingUtils.kt
+++ app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/BindingUtils.kt
@@ -16,4 +16,39 @@
 
 package com.example.android.trackmysleepquality.sleeptracker
 
-// TODO (01) Define BindingAdapters for sleepImage, sleepDurationFormatted, and sleepQualityString.
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.databinding.BindingAdapter
+import com.example.android.trackmysleepquality.R
+import com.example.android.trackmysleepquality.convertDurationToFormatted
+import com.example.android.trackmysleepquality.convertNumericQualityToString
+import com.example.android.trackmysleepquality.database.SleepNight
+
+@BindingAdapter("setDurationFormatted")
+fun TextView.setDurationFormatted(item: SleepNight?) {
+    item?.let {
+        text = convertDurationToFormatted(it.startTimeMilli, it.endTimeMilli, context.resources)
+    }
+}
+
+@BindingAdapter("setStringQuality")
+fun TextView.setStringQuality(item: SleepNight?) {
+    item?.let {
+        text = convertNumericQualityToString(it.sleepQuality, context.resources)
+    }
+}
+
+@BindingAdapter("setImage")
+fun ImageView.setImage(item: SleepNight?) {
+    item?.let {
+        setImageResource(when(it.sleepQuality){
+            0 -> R.drawable.ic_sleep_0
+            1 -> R.drawable.ic_sleep_1
+            2 -> R.drawable.ic_sleep_2
+            3 -> R.drawable.ic_sleep_3
+            4 -> R.drawable.ic_sleep_4
+            5 -> R.drawable.ic_sleep_5
+            else -> R.drawable.ic_sleep_active
+        })
+    }
+}
